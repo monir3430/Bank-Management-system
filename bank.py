bdb287038a7075e9c654c0f0804931a65162004a
monir3430 <miamonir3430@gmail.com>
2024-06-06 15:25:48 +0600

    user class set

--- bank.py
+++ bank.py
@@ -1,5 +1,7 @@
 class Bank:
-    def __init__(self):
+    
+    def __init__(self, bank_name):
+        self.Bank_name = bank_name
         self.clients = {}
         self.admins = {}
         self.t_balance = 0
@@ -10,6 +12,16 @@ class Bank:
         if user_type =='user':
             if user_name not in self.clients:
                 self.clients[user_name] = User(user_name, primary_deposit)
+                self.t_balance += primary_deposit
+                print(f"Welcome to {self.Bank_name} as a new client")
+
+            else:
+                print(f"{user_name} already existed")
+
+        else:
+            print("Invalid user type")
+
+
 
 
 
@@ -22,4 +34,4 @@ class User:
     def __init__(self, user_name, balance = 0):
         self.user_name = user_name
         self.balance = balance
-        self.Transaction_history = []
+        self.transaction_history = []
