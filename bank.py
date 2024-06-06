d5faee2d54a4637467a4736271fab92646703d63
monir3430 <miamonir3430@gmail.com>
2024-06-06 15:05:26 +0600

    user class

--- bank.py
+++ bank.py
@@ -5,3 +5,21 @@ class Bank:
         self.t_balance = 0
         self.t_loan = 0
         self.loan_enable = True
+
+    def create_account(self, user_type,user_name, primary_deposit=0):
+        if user_type =='user':
+            if user_name not in self.clients:
+                self.clients[user_name] = User(user_name, primary_deposit)
+
+
+
+
+
+
+
+
+class User:
+    def __init__(self, user_name, balance = 0):
+        self.user_name = user_name
+        self.balance = balance
+        self.Transaction_history = []
